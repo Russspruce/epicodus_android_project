1f3d4f830943a5a1328f2de73a3edb4e96928196
Andrew Russo <russspruce@gmail.com>
2016-07-05 02:48:01 -0700

    Added login info and a profile page start that will be where the user goes when logged in.

--- app/src/main/java/com/epicodus/andrewrusso/elforge/MainActivity.java
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/MainActivity.java
@@ -1,13 +1,40 @@
 package com.epicodus.andrewrusso.elforge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind (R.id.loginButton) Button mLoginButton;
+    @Bind (R.id.usernameText) EditText mUsernameText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            String username = mUsernameText.getText().toString();
+            Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+            intent.putExtra("username", username);
+            startActivity(intent);
+
+
+        }
     }
 }
+
---
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.epicodus.andrewrusso.elforge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Bind (R.id.usernameTextView) TextView mUsernameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+    }
+}
