100013b4d1961895b6b541dd5be012568c5ddbef
Andrew Russo <russspruce@gmail.com>
2016-07-08 02:49:33 -0700

    Created connection and currently working on creating a list with search within the android app that would allow the user to search for the games they wish to have people play with them.

---
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/GamesActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.andrewrusso.elforge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games);
+    }
+}
--- app/src/main/java/com/epicodus/andrewrusso/elforge/ProfileActivity.java
+++ app/src/main/java/com/epicodus/andrewrusso/elforge/ProfileActivity.java
@@ -1,19 +1,41 @@
 package com.epicodus.andrewrusso.elforge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
     @Bind (R.id.usernameTextView) TextView mUsernameTextView;
+    @Bind (R.id.gameSearchButton) Button mGameSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
-    }
+
+    mGameSearchButton.setOnClickListener(this);
+
+    Intent intent = getIntent();
+    String username = intent.getStringExtra("username");
+
+    mUsernameTextView.setText("Welcome back, " + username +"!");
+}
+
+    @Override
+    public void onClick(View v) {
+        if (v == mGameSearchButton) {
+            Intent intent = new Intent(ProfileActivity.this,GamesActivity.class);
+            startActivity(intent);
+
+
+        }
 }
